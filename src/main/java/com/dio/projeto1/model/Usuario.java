9b82cf57526072bff84821380b9bbac38b8b49cd
MariaCarolina96 <mcaa96@hotmail.com>
2021-09-14 01:03:38 -0300

    Projeto 01

---
+++ src/main/java/com/dio/projeto1/model/Usuario.java
@@ -0,0 +1,35 @@
+package com.dio.projeto1.model;
+
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+@Audited
+public class Usuario {
+    @Id
+    private Long id;
+    @ManyToOne // Many = vários usuários podem estar relacionadas a uma categoria.
+    private CategoriaUsuario categoriaUsuario;
+    private String nome;
+    @ManyToOne // Uma empresa para vários usuários
+    private Empresa empresa;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    @ManyToOne
+    private JornadaTrabalho jornadaTrabalho;
+    private BigDecimal tolerancia;
+    private LocalDateTime inicioJornada;
+    private LocalDateTime finalJornada;
+}
